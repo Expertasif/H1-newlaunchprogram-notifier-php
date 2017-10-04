83d359b6c4ad7d20f968958c45fe8cf937abb3c1
Evgeniy Yakovchuk <yakovchuk.yevhen@kpnu.edu.ua>
2017-10-04 16:12:28 +0300

    Remove some unnecessary empty spaces

--- test.php
+++ test.php
@@ -4,9 +4,9 @@ $sender="sender@domain.com"; //ideally the existing mailbox on your domain, wher
 
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, 'https://hackerone.com/programs/search?query=bounties%3Ayes&sort=published_at%3Adescending&page=1');
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept-Language: ru,en-US;q=0.8,en;q=0.6,uk;q=0.4','Accept: application/json, text/javascript, */*; q=0.01', 'X-Requested-With: XMLHttpRequest', 'Referer: https://hackerone.com/directory?query=bounties%3Ayes&sort=published_at%3Adescending&page=1', 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-	curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept-Language: ru,en-US;q=0.8,en;q=0.6,uk;q=0.4','Accept: application/json, text/javascript, */*; q=0.01', 'X-Requested-With: XMLHttpRequest', 'Referer: https://hackerone.com/directory?query=bounties%3Ayes&sort=published_at%3Adescending&page=1', 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl, CURLOPT_TIMEOUT, 60);
 $string=curl_exec($curl);
 $pos=strpos($string,"name");
 $pos2=strpos($string,"\"",$pos+7);
